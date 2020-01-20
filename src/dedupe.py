61d6c9a61f742f80b0ae5be0b77b2ce5187458eb
lianjian zhou <1126185240@qq.com>
2020-01-20 16:44:24 +0800

    ??

--- src/dedupe.py
+++ src/dedupe.py
@@ -6,24 +6,17 @@ from eq import chn_eq, str_eq
 #SEARCH_RANGE=20
 
 def dedupe(xlsx_name):
-    df = DataFrame(pd.read_excel(xlsx_name))
-    df['duped'] = False
+    data = DataFrame(pd.read_excel(xlsx_name)).values
+    #data2 = 
 
-    data = df.values
-
-    for i in range(len(data) - 1):
-        if i > 0:
-            start = max(0, i - 20)
-            print(i, start)
-            for j, test_item in enumerate(df[start:i]):
-                print(item)
-                if len(item['信息标题']) < 10:
-                    if str_eq(item['招标编号'], test_item['招标编号']):
-                        item['duped'] = True
-                elif len(item['信息标题']) >= 10:
-                    if chn_eq(item['信息标题'], test_item['信息标题']):
-                        item['duped'] = True
-    
-    df.to_excel('processed_' + xlsx_name)
+    for i in range(1, len(data)):
+        print(data[i])
+        start = max(0, i - 20)
+        for j in range(start, i):
+                if chn_eq(data[i][1], data[j][1]):
+                    data[i][7] = j
+                if str_eq(data[i][3], data[j][3]):
+                    data[i][8] = j
+    DataFrame(data).to_excel('processed_' + xlsx_name)
 
 dedupe('o_2000.xlsx')
--- src/eq.py
+++ src/eq.py
@@ -1,28 +1,53 @@
 import jieba
 import json
 
+#招标笼统词
 junk_word = json.load(open('./old/junk.json', encoding='utf-8'))
 
+#行业笼统词
+broad_word = json.load(open('./old/broad.json', encoding='utf-8'))
+
+def too_broad(s):
+    s = list(jieba.cut(str(s)))
+    count = 0
+    
+    for i in range(len(s)):
+        if (s[i] in broad_word) or (s[i] in junk_word):
+            count = count + 1
+    return count / len(s) > 0.95
+
 def chn_eq(c1, c2):
+    c1 = str(c1)
+    c2 = str(c2)
+
     if c1 == c2:
         return True
+    if too_broad(c1) or too_broad(c2):
+        return False
+
+    if(len(c1) > len(c2)):
+        tmp = c1
+        c1 = c2
+        c2 = tmp
+        
     shortli = list(jieba.cut(c1))
     longli = list(jieba.cut(c2))
     count = 0
 
-    if(len(shortli) > len(longli)):
-        tmp = shortli
-        shortli = longli
-        longli = tmp
-
     shortlen = len(shortli)
-
+    equal = []
+    remain = []
     for index, word in enumerate(shortli):
         if word in junk_word:
             shortlen = shortlen - 1
         elif word in longli:
-            count = count + 1 
+            count = count + 1
+            equal.append(word)
+        else:
+            remain.append(word)
     return count == shortlen
 
 def str_eq(s1, s2):
-    return (s1 in s2) or (s2 in s1)
+    s1 = str(s1)
+    s2 = str(s2)
+    return s1 != 'nan' and s2 != 'nan' and ((s1 in s2) or (s2 in s1))
